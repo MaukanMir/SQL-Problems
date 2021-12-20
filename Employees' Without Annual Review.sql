7f6300f6afa80e2f60f4451deaeecb1a915d225f
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-12-19 21:20:05 -0500

    Create Employees' Without Annual Review.sql

---
+++ Employees' Without Annual Review.sql
@@ -0,0 +1,7 @@
+Easy Uber Active Interview Questions ID 2043
+Interview Question Date: May 2021
+Employees' Without Annual Review
+Return all employees who have never had an annual review. Your output should include the employee's first name, last name, hiring date, and termination date. List the most recently hired employees first.
+
+
+select * from uber_annual_review;
