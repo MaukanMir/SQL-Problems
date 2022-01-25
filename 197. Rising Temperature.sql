50cb195edca0422b2da562c43fafc664db3335f3
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-25 16:45:19 -0500

    Create 197. Rising Temperature.sql

---
+++ 197. Rising Temperature.sql
@@ -0,0 +1,55 @@
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+
+Success
+Details 
+Runtime: 497 ms, faster than 38.59% of MySQL online submissions for Rising Temperature.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Rising Temperature.
+
+SELECT weather.id as 'id'
+from weather
+    JOIN
+    weather w on DATEDIFF(weather.recordDate, w.recordDate) =1
+    and weather.Temperature > w.temperature
