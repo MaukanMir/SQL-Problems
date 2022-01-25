7900dabe566aa4174e3d502175c3041f3a66406a
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-25 16:29:09 -0500

    Create 182. Duplicate Emails.sql

---
+++ 182. Duplicate Emails.sql
@@ -0,0 +1,52 @@
+SQL Schema
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to report all the duplicate emails.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+
+Success
+Details 
+Runtime: 330 ms, faster than 59.53% of MySQL online submissions for Duplicate Emails.
+Memory Usage: 0B, less than 100.00% of MySQL online submissions for Duplicate Emails.
+
+
+select email from
+(Select email,Count(email) as total
+from Person
+GROUP BY email)stat
+where total >1
