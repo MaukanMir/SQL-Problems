6983112606c3ad7d1a3a9fef46ae66ca50390e26
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-26 17:16:04 -0500

    Create Total AdWords Earnings.sql

---
+++ Total AdWords Earnings.sql
@@ -0,0 +1,10 @@
+
+
+select business_type, sum(adwords_earnings) from google_adwords_earnings as earnings
+group by business_type
+
+
+Easy Google Interview Questions ID 10164
+Interview Question Date: Jul 2020
+Total AdWords Earnings
+Find the total AdWords earnings for each business type. Output the business types along with the total earnings.
