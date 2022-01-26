92265539a8404a8ac8a21b71f797aba9d98171bd
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-26 17:18:52 -0500

    Create Find workers with an even number for worker id.sql

---
+++ Find workers with an even number for worker id.sql
@@ -0,0 +1,8 @@
+
+
+Easy Amazon General Practice ID 10137
+Find workers with an even number for worker id
+Find workers and their corresponding information from the table with an even number for their worker id.
+
+select * from worker 
+where worker_id %2 = 0
