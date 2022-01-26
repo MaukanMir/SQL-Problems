7f559c2c129f3850a433edf99c455677f9e96c6d
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-26 17:46:49 -0500

    Create Highest Salary.sql

---
+++ Highest Salary.sql
@@ -0,0 +1,9 @@
+Easy Amazon General Practice ID 9870
+Highest Salary
+Find the worker who earns the highest salary. Output the worker's first name along with the salary.
+
+
+
+select first_name, salary from worker
+order by salary desc
+limit 2
