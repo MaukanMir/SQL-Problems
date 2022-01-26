e204b56d2ccd580c83cdc3d4e79db3aeff04ff71
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-26 17:19:56 -0500

    Create Find workers with an odd number for worker id.sql

---
+++ Find workers with an odd number for worker id.sql
@@ -0,0 +1,7 @@
+Easy Amazon General Practice ID 10136
+Find workers with an odd number for worker id
+Find workers and their corresponding information from the table with an odd number for their worker id.
+
+
+select * from worker
+where worker_id %2 !=0
