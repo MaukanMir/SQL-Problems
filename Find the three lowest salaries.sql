3ad9d635808231f49b403a10b20dddf11b37ea49
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 18:42:53 -0500

    Create Find the three lowest salaries.sql

---
+++ Find the three lowest salaries.sql
@@ -0,0 +1,14 @@
+
+Easy Amazon General PracticeID 9867
+Find the three lowest salaries
+Find the three lowest salaries.
+Order records based on the salary in ascending order.
+
+
+
+select distinct salary from worker
+order by salary
+limit 3
+
+
+
