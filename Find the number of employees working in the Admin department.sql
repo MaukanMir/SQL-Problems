858a21cfdb525073f4fa80a22f2e8dd41f6b986c
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 18:52:56 -0500

    Create Find the number of employees working in the Admin department.sql

---
+++ Find the number of employees working in the Admin department.sql
@@ -0,0 +1,7 @@
+Easy Amazon General Practice ID 9845
+Find the number of employees working in the Admin department
+Find the number of employees working in the Admin department.
+
+
+select count(department) from worker
+where department = 'Admin'
