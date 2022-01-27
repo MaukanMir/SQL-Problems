d600a6f61291dd395d3bad6f8dc2ccb04f5ccf0d
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 18:54:42 -0500

    Create Find all workers whose salary lies between 100000 and 500000.sql

---
+++ Find all workers whose salary lies between 100000 and 500000.sql
@@ -0,0 +1,8 @@
+
+Easy Amazon General Practice ID 9843
+Find all workers whose salary lies between 100000 and 500000
+Find all workers whose salary lies between 100000 and 500000.
+
+
+select * from worker
+where salary between 100000 and 500000
