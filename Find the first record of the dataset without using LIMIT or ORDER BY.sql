9c55b1f9f38624820e94f3ae424175e07a8bb073
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 18:45:21 -0500

    Create Find the first record of the dataset without using LIMIT or ORDER BY.sql

---
+++ Find the first record of the dataset without using LIMIT or ORDER BY.sql
@@ -0,0 +1,6 @@
+Easy AmazonGeneral Practice ID 9863
+Find the first record of the dataset without using LIMIT or ORDER BY
+Find the first record of the dataset without using LIMIT or ORDER BY.
+
+select * from worker
+where worker_id in (select  min(worker_id) from worker)
