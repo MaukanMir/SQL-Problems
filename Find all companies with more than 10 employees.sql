b99dd9741eec8dafb655cbc82d36eb6807d329be
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:24:55 -0500

    Create Find all companies with more than 10 employees.sql

---
+++ Find all companies with more than 10 employees.sql
@@ -0,0 +1,9 @@
+
+
+Easy Google General Practice ID 9807
+Find all companies with more than 10 employees
+Find all companies with more than 10 employees.
+
+
+select distinct  * from google_adwords_earnings
+where n_employees >10
