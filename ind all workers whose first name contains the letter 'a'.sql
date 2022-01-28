6d499c7840b9ba3853d0949ba944a3dbc53bb24f
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:03:04 -0500

    Create ind all workers whose first name contains the letter 'a'.sql

---
+++ ind all workers whose first name contains the letter 'a'.sql
@@ -0,0 +1,6 @@
+Easy Amazon General PracticeI D 9840
+Find all workers whose first name contains the letter 'a'
+Find all workers whose first name contains the letter 'a'.
+
+select * from worker
+where first_name ilike '%a%'
