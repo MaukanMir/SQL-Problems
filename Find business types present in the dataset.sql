6b48760adccf0b1896d0999df70e727a4f662245
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:18:09 -0500

    Create Find business types present in the dataset.sql

---
+++ Find business types present in the dataset.sql
@@ -0,0 +1,7 @@
+
+
+Easy Google General Practice ID 9808
+Find business types present in the dataset
+Find business types present in the dataset.  
+
+select distinct business_type from google_adwords_earnings
