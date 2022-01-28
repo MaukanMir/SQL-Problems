69b156fd7245ce6f4b110cabff3637bd93baac89
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:07:46 -0500

    Create Sort workers in ascending order by the first name and in descending order by department name.sql

---
+++ Sort workers in ascending order by the first name and in descending order by department name.sql
@@ -0,0 +1,9 @@
+
+
+
+Easy Amazon General Practice ID 9836
+Sort workers in ascending order by the first name and in descending order by department name
+Sort workers in ascending order by the first name and in descending order by department name.
+
+select * from worker
+order by first_name, department desc
