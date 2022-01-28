bf58a8b6b18fc959227c1f623be22bad0af404ea
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:04:25 -0500

    Create Find all workers that work in the Admin department.sql

---
+++ Find all workers that work in the Admin department.sql
@@ -0,0 +1,6 @@
+Easy Amazon General Practice ID 9839
+Find all workers that work in the Admin department
+Find all workers that work in the Admin department
+
+select * from worker 
+where department = 'Admin'
