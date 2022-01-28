1997ee10f1cd405646af03f226e70a18a12fd492
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:27:38 -0500

    Create Find drafts which contains the word 'optimism'.sql

---
+++ Find drafts which contains the word 'optimism'.sql
@@ -0,0 +1,7 @@
+Eas yGoogle General Practice ID 9805
+Find drafts which contains the word 'optimism'
+Find drafts which contains the word 'optimism'.
+
+select * from google_file_store
+
+where contents ilike '%optimism%' and filename= 'draft2.txt'
