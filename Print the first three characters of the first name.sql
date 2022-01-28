043d4a3d1eb73c959ace687fd1d8676939ac896a
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:15:42 -0500

    Create Print the first three characters of the first name.sql

---
+++ Print the first three characters of the first name.sql
@@ -0,0 +1,7 @@
+
+Easy Amazon General PracticeID 9828
+Print the first three characters of the first name
+Print the first three characters of the first name.
+Table: worker
+
+select substring(first_name, 1,3) from worker;
