5f6545bfe65f941cd816d557e31e53509316cba3
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:34:18 -0500

    Create Find the number of searches on Airbnb.sql

---
+++ Find the number of searches on Airbnb.sql
@@ -0,0 +1,6 @@
+Easy Airbnb General Practice ID 9760
+Find the number of searches on Airbnb
+Find the number of people that search for hosts on Airbnb.
+
+select count(distinct id_user) from airbnb_searches;
+
