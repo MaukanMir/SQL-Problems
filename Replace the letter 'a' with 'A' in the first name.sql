3245cff63f0b77c2223049411afb348a28dcde1a
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:11:52 -0500

    Create Replace the letter 'a' with 'A' in the first name.sql

---
+++ Replace the letter 'a' with 'A' in the first name.sql
@@ -0,0 +1,5 @@
+Easy Amazon General Practice ID 9833
+Replace the letter 'a' with 'A' in the first name
+Replace the letter 'a' with 'A' in the first name.
+
+select  replace(first_name,'a', 'A')  from worker
