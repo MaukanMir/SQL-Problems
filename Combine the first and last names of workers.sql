9798124159e4d76f19240b7d165e415c61f30a65
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:08:58 -0500

    Create Combine the first and last names of workers.sql

---
+++ Combine the first and last names of workers.sql
@@ -0,0 +1,10 @@
+
+
+
+Easy Amazon General Practice ID 9834
+Combine the first and last names of workers
+Combine the first and last names of workers with a space inbetween.
+
+
+select concat(first_name, ' ', last_name) as full_name from worker;
+
