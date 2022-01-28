a470d826ea0b1eb64b7a49bbb7efa30d85323264
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:13:50 -0500

    Create Print the first name after removing white spaces from the right side.sql

---
+++ Print the first name after removing white spaces from the right side.sql
@@ -0,0 +1,8 @@
+
+
+Easy Amazon General Practice ID 9830
+Print the first name after removing white spaces from the right side
+Print the first name after removing white spaces from the right side.
+
+
+select trim(first_name, ' ') from worker;
