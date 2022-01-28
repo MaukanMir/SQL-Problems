312d6573918811a2d13541ac0076986e77357d52
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:05:38 -0500

    Create Find details of workers excluding those with the first name 'Vipul' or 'Satish'.sql

---
+++ Find details of workers excluding those with the first name 'Vipul' or 'Satish'.sql
@@ -0,0 +1,8 @@
+
+
+Eas yAmazon General Practice ID 9838
+Find details of workers excluding those with the first name 'Vipul' or 'Satish'
+Find details of workers excluding those with the first name 'Vipul' or 'Satish'
+
+select * from worker
+where first_name not in ('Vipul', 'Satish')
