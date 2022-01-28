b175f2d9ac9f514490339a5636c6ad45a0476bc9
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-27 19:01:18 -0500

    Create Find all workers whose first name ends with the letter 'a'.sql

---
+++ Find all workers whose first name ends with the letter 'a'.sql
@@ -0,0 +1,7 @@
+Easy AmazonGeneral Practice ID 9841
+Find all workers whose first name ends with the letter 'a'
+Find all workers whose first name ends with the letter 'a'.
+
+
+select * from worker
+where first_name ilike '%a'
