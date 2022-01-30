d4ab79d6e6563c13599b2cd07affee7308d083b2
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-29 21:50:48 -0500

    Create Find all neighborhoods present in this dataset.sql

---
+++ Find all neighborhoods present in this dataset.sql
@@ -0,0 +1,6 @@
+Easy Airbnb General Practice ID 9626
+Find all neighborhoods present in this dataset
+Find all neighbourhoods present in this dataset.
+
+
+select distinct neighbourhood from airbnb_search_details
