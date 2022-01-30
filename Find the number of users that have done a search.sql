df73d12fa70c89ce438e32678178c8a965647335
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-29 21:15:52 -0500

    Create Find the number of users that have done a search.sql

---
+++ Find the number of users that have done a search.sql
@@ -0,0 +1,6 @@
+Easy Airbnb General Practice ID 9641
+Find the number of users that have done a search
+How many unique users have performed a search?
+
+
+select count( distinct id_user) from airbnb_searches
