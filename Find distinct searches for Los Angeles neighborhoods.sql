ab04111ef5d61c7e6148f40bef155e7c1163fce9
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-31 16:31:54 -0500

    Create Find distinct searches for Los Angeles neighborhoods.sql

---
+++ Find distinct searches for Los Angeles neighborhoods.sql
@@ -0,0 +1,6 @@
+Easy Airbnb General Practice ID 9618
+Find distinct searches for Los Angeles neighborhoods
+Find searches for Los Angeles neighborhoods.
+
+select distinct neighbourhood from airbnb_search_details
+where city = 'LA'
