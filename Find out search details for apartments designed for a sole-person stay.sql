2c9d33f29b64cb7e4e1dfd0ecc59875ce1327bf1
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-31 16:42:26 -0500

    Create Find out search details for apartments designed for a sole-person stay.sql

---
+++ Find out search details for apartments designed for a sole-person stay.sql
@@ -0,0 +1,6 @@
+Easy Airbnb General Practice ID 9615
+Find out search details for apartments designed for a sole-person stay
+Find the search details made by people who searched for apartments designed for a single-person stay.
+
+select* from airbnb_search_details
+where accommodates =1 and property_type ='Apartment'
