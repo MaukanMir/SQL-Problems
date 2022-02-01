fe5fc7c77ba85fa3ea4606c362390b1809636da6
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-31 20:31:38 -0500

    Create Find the average number of streams across all songs.sql

---
+++ Find the average number of streams across all songs.sql
@@ -0,0 +1,5 @@
+Easy Spotify General Practice ID 9996
+Find the average number of streams across all songs
+Find the average number of streams across all songs.
+
+select avg(streams) from spotify_worldwide_daily_song_ranking;
