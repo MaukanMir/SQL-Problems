5956d20280970ec2f609c13bf99e68c4c0ea912b
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-31 20:00:04 -0500

    Create Cast stars column values to integer and return with all other column values.sql

---
+++ Cast stars column values to integer and return with all other column values.sql
@@ -0,0 +1,8 @@
+Easy Yelp General Practice ID 10056
+Cast stars column values to integer and return with all other column values
+Cast stars column values to integer and return with all other column values.
+
+
+select * 
+from yelp_reviews
+WHERE stars != '?'
