5ca6f846734ab05909ced99af51c0e7287033e05
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-31 20:19:57 -0500

    Create Find the advertising channel where Uber spent more than 100k USD in 2019.sql

---
+++ Find the advertising channel where Uber spent more than 100k USD in 2019.sql
@@ -0,0 +1,9 @@
+Easy Uber General Practice ID 10002
+Find the advertising channel where Uber spent more than 100k USD in 2019
+Find the advertising channel where Uber spent more than 100k USD in 2019.
+
+
+select distinct  advertising_channel from uber_advertising
+
+
+where money_spent >= 100000 and year ='2019'
