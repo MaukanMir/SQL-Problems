8aea61b7b4dceddd0adf0035c5575ea5c0ad7f33
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-01 20:38:37 -0500

    Create Find the base pay for Police Captains.sql

---
+++ Find the base pay for Police Captains.sql
@@ -0,0 +1,7 @@
+Easy City of San Francisco General Practice ID 9972
+Find the base pay for Police Captains
+Find the base pay for Police Captains.
+Output the employee name along with the corresponding base pay.
+
+select employeename, basepay from sf_public_salaries
+where jobtitle ilike '%captain%'
