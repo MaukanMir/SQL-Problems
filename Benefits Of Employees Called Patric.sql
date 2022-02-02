2334595927dc4048e24b13f10b87a5cc710ac188
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-01 20:31:10 -0500

    Create Benefits Of Employees Called Patric.sql

---
+++ Benefits Of Employees Called Patric.sql
@@ -0,0 +1,7 @@
+Easy City of San Francisco General Practice ID 9974
+Benefits Of Employees Called Patric
+Find benefits that people with the name 'Patrick' have.
+Output the employee name along with the corresponding benefits.
+
+select employeename, benefits from sf_public_salaries
+where employeename ilike '%patrick%'
