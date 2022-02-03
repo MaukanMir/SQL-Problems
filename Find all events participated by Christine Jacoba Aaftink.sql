14d9ab66f1552d8f2d430209a75396fd99f51087
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-02 20:08:02 -0500

    Create Find all events participated by Christine Jacoba Aaftink.sql

---
+++ Find all events participated by Christine Jacoba Aaftink.sql
@@ -0,0 +1,8 @@
+Easy ESPN General Practice ID 9935
+Find all events participated by Christine Jacoba Aaftink
+Find all events participated by Christine Jacoba Aaftink.
+Output unique values only.
+
+select distinct event from olympics_athletes_events
+where name = 'Christine Jacoba Aaftink'
+
