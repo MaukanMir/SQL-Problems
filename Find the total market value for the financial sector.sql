f807a842b04c33b5e853b69c6d064f3e08c2ee70
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-02 20:45:37 -0500

    Create Find the total market value for the financial sector.sql

---
+++ Find the total market value for the financial sector.sql
@@ -0,0 +1,6 @@
+Easy Forbes General Practice ID 9800
+Find the total market value for the financial sector
+Find the total market value for the financial sector.
+
+select sum(marketvalue) from forbes_global_2010_2014
+where sector = 'Financials'
