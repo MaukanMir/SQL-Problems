be45c79fdd07cc398ea5d5170d7911f463a76673
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-02 20:47:48 -0500

    Create Find the average profit for major banks.sql

---
+++ Find the average profit for major banks.sql
@@ -0,0 +1,6 @@
+Easy Forbes General Practice ID 9798
+Find the average profit for major banks
+Find the average profit for major banks.
+
+select avg(profits) from forbes_global_2010_2014
+where industry ='Major Banks'
