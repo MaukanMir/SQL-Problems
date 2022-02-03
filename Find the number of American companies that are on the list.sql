614b7f3cec203275174fcf81eb3d83fdf2bfb928
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-02 20:43:44 -0500

    Create Find the number of American companies that are on the list.sql

---
+++ Find the number of American companies that are on the list.sql
@@ -0,0 +1,6 @@
+Easy Forbes General Practice ID 9801
+Find the number of American companies that are on the list
+Find the number of American companies that are on the list.
+
+select count(company) from forbes_global_2010_2014
+where country ='United States'
