bf8a19ac218e5cad326845f8cdf22149bcbb5df0
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-02 20:01:49 -0500

    Create Find all athletes who were older than 40 years when they won either Bronze or Silver medals.sql

---
+++ Find all athletes who were older than 40 years when they won either Bronze or Silver medals.sql
@@ -0,0 +1,11 @@
+
+
+
+
+Easy ESPN General Practice ID 9937
+Find all athletes who were older than 40 years when they won either Bronze or Silver medals
+Find all athletes who were older than 40 years when they won either Bronze or Silver medals.
+
+
+select name from olympics_athletes_events
+where age > 40 and medal in ('Bronze','Silver')
