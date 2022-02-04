eb1aa21a20abbd2b9bb7b630b1645d771aad3719
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-03 20:17:35 -0500

    Create Find finance companies based in Europe and Asia.sql

---
+++ Find finance companies based in Europe and Asia.sql
@@ -0,0 +1,6 @@
+Easy Forbes General Practice ID 9678
+Find finance companies based in Europe and Asia
+Find companies in the financial sector based on Europe and Asia.
+
+select company from forbes_global_2010_2014
+where continent in ('Asia', 'Europe') and sector = 'Financials'
