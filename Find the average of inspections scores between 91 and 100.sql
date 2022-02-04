aa20bdd867c11e895bc913fa6cccc5c4ff3d87b2
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-03 19:56:19 -0500

    Create Find the average of inspections scores between 91 and 100.sql

---
+++ Find the average of inspections scores between 91 and 100.sql
@@ -0,0 +1,7 @@
+Easy City of Los Angeles General Practice ID 9707
+Find the average of inspections scores between 91 and 100
+Find the mean of inspections scores between 91 and 100.
+Assuming that the scores are normally distributed.
+
+select avg(score) from los_angeles_restaurant_health_inspections
+where score >=91
