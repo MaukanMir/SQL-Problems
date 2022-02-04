7dcd9c11585ef073cc1fae6a04fd7263f10d5a3f
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-03 20:06:28 -0500

    Create Churro Activity Date.sql

---
+++ Churro Activity Date.sql
@@ -0,0 +1,8 @@
+Easy City of Los Angeles General PracticeID 9688
+Churro Activity Date
+Find the activity date and the pe_description of facilities with the name 'STREET CHURROS' and with a score of less than 95 points.
+
+
+
+select activity_date, pe_description from los_angeles_restaurant_health_inspections
+where facility_name ilike '%STREET CHURROS%'  and score <95
