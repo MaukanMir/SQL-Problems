6c566d593655a5392ac8029217c4b0412f1bf8f8
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-03 20:13:58 -0500

    Create Find the profit to sales ratio of Royal Dutch Shell.sql

---
+++ Find the profit to sales ratio of Royal Dutch Shell.sql
@@ -0,0 +1,7 @@
+Easy Forbes General Practice ID 9679
+Find the profit to sales ratio of Royal Dutch Shell
+What is the profit to sales ratio (profit/sales) of Royal Dutch Shell?
+Output the result along with the company name.
+
+select company,profits/sales from forbes_global_2010_2014
+where company = 'Royal Dutch Shell'
