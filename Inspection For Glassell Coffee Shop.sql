8a0bb3e22095d8c3e2520a9311f71d09b8221a91
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-03 20:03:43 -0500

    Create Inspection For Glassell Coffee Shop.sql

---
+++ Inspection For Glassell Coffee Shop.sql
@@ -0,0 +1,7 @@
+Easy City of Los Angeles General Practice ID 9689
+Inspection For Glassell Coffee Shop
+Find all inspection details made for facilities owned by 'GLASSELL COFFEE SHOP LLC'.
+
+
+select * from los_angeles_restaurant_health_inspections
+where owner_name ilike '%GLASSELL COFFEE SHOP LLC%'
