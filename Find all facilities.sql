1cfb1fb86cce6471dbcf407db11d3adfab61eb3d
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-03 20:00:22 -0500

    Create Find all facilities.sql

---
+++ Find all facilities.sql
@@ -0,0 +1,7 @@
+Easy City of Los Angeles General Practice ID 9691
+Facility Zip Codes
+Find all facilities with the zip code 90049, 90034, or 90045.
+
+
+select distinct facility_name from los_angeles_restaurant_health_inspections
+where facility_zip in ('90049','90034','90045')
