6b821e0a906b6c3a401934afdf464db0d7f4651d
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-03 19:53:32 -0500

    Create Find all businesses which have a phone number listed.sql

---
+++ Find all businesses which have a phone number listed.sql
@@ -0,0 +1,11 @@
+
+
+
+
+
+Easy City of San FranciscoGeneral Practice ID 9718
+Find all businesses which have a phone number listed
+Find all businesses which have a phone number.
+
+select distinct business_name from sf_restaurant_health_violations
+where business_phone_number is not null
