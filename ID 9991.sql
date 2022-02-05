c49f576cbd9be020538cabda948436695377627c
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-05 17:04:13 -0500

    Create ID 9991.sql

---
+++ ID 9991.sql
@@ -0,0 +1,7 @@
+Medium Spotif yGeneral Practice ID 9991
+Top Ranked Songs
+Find songs that have ranked in the top position. Output the track name and the number of times it ranked at the top. Sort your records by the number of times the song was in the top position in descending order.
+select trackname, count(*) as toptop from spotify_worldwide_daily_song_ranking
+where position =1
+group by trackname
+order by toptop desc
