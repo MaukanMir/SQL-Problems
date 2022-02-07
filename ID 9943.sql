5e66c4df24e2d64e9a8030a3ff99fbc3eec014bf
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-07 18:24:43 -0500

    Create ID 9943.sql

---
+++ ID 9943.sql
@@ -0,0 +1,5 @@
+Medium ESPN General PracticeI D 9943
+Winter Olympics Events List By Height
+
+Find the lowest, average, and the highest ages of athletes across all Olympics.
+select min(age), avg(age), max(age) from olympics_athletes_events;
