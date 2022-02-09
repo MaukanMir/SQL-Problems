59d8491667892598911755555e037a5a60d18058
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-08 22:56:04 -0500

    Create ID 9922.sql

---
+++ ID 9922.sql
@@ -0,0 +1,8 @@
+Medium City of San Francisco General Practice ID 9922
+Highest Checkouts
+Find the number of patrons that have made the highest checkouts up to 10 (excluding 10). 
+Output the number of patrons along with the corresponding total checkouts. Sort records based on the total checkouts in descending order.
+
+select count(patron_type_code), total_checkouts from library_usage
+where total_checkouts <10
+group by total_checkouts
