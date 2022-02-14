d676775f3bf50d2ab1773ce56bcb72addc174356
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-14 09:18:36 -0500

    Create ID 9616.sql

---
+++ ID 9616.sql
@@ -0,0 +1,7 @@
+Easy Airbnb General Practice ID 9616
+Apartments in New York City and Harlem
+Find the search details of 50 apartment searches the Harlem neighborhood of New York City.
+
+select * from airbnb_search_details
+where neighbourhood ='Harlem' and property_type ='Apartment'
+limit 50
