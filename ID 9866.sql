46857931b3d670f43f95662127b6238ee228bb73
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-27 10:48:17 -0500

    Update ID 9866.sql

--- ID 9866.sql
+++ ID 9866.sql
@@ -6,3 +6,8 @@ Order records based on the salary in descending order.
 select salary from
 (select distinct salary, dense_rank() over(order by salary desc) as rnk from worker)stat
 where rnk <=3
+
+
+select distinct salary from worker 
+order by salary desc
+limit 3
