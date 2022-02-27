c367d534d7da6229f615d7006effbaf168b4da3d
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-27 11:07:25 -0500

    Update ID 9853.sql

--- ID 9853.sql
+++ ID 9853.sql
@@ -6,3 +6,11 @@ Find the top 5 highest paid workers. Output all information about workers whose 
 select distinct salary,worker_id, first_name,last_name,joining_date,department from worker
 order by salary desc
 limit 10
+
+
+
+with cte as (
+select *, dense_rank() over(order by salary desc)as rnk from worker)
+
+select worker_id, first_name, last_name, salary, joining_date, department from cte
+where rnk <=10
