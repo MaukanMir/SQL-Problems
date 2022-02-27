ce9e98c5aa931f2902eea6d78b15bb5f24d0f5bf
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-27 10:47:26 -0500

    Create ID 9866.sql

---
+++ ID 9866.sql
@@ -0,0 +1,8 @@
+Easy Amazon General Practice ID 9866
+Find the three highest salaries
+Find the three highest salaries.
+Order records based on the salary in descending order.
+
+select salary from
+(select distinct salary, dense_rank() over(order by salary desc) as rnk from worker)stat
+where rnk <=3
