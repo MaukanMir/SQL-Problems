af52b893bd015ec9aa76c004f160e68f6b32d98c
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-28 10:37:18 -0500

    Create ID 10078.sql

---
+++ ID 10078.sql
@@ -0,0 +1,9 @@
+Medium Airbnb General Practice ID 10078
+Find matching hosts and guests in a way that they are both of the same gender and nationality
+Find matching hosts and guests pairs in a way that they are both of the same gender and nationality.
+Output the host id and the guest id of matched pair.
+
+
+select distinct host_id, guest_id from airbnb_hosts as ah
+inner join airbnb_guests as ag on ag.nationality = ah.nationality 
+where ag.nationality = ah.nationality and ag.gender = ah.gender
