6a85d9ec150ac4a06b5b5411782d909e6a0d60ec
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-03-01 10:13:24 -0500

    Create ID 10178.sql

---
+++ ID 10178.sql
@@ -0,0 +1,9 @@
+Medium Yelp General Practice ID 10178
+Businesses Open On Sunday
+Find the number of businesses that are open on Sundays. Output the slot of operating hours along with the corresponding number of businesses open during those time slots. Order records by total number of businesses opened during those hours in descending order.
+
+select distinct sunday, count(ybh.business_id) as total from yelp_business_hours as ybh
+join yelp_business as yb on yb.business_id = ybh.business_id
+where is_open = 1 and sunday is not null
+group by sunday
+order by total desc
