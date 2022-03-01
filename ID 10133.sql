a2b9590e012f88f9864612770211a0fac1e9205b
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-03-01 10:08:18 -0500

    Update ID 10133.sql

--- ID 10133.sql
+++ ID 10133.sql
@@ -3,3 +3,8 @@ Requests Acceptance Rate
 Find the acceptance rate of requests which is defined as the ratio of accepted contacts vs all contacts. Multiply the ratio by 100 to get the rate.
 
 select (count(ts_accepted_at)/count(ts_contact_at)::float) * 100 from airbnb_contacts
+
+
+ with cte as(select (count(ts_accepted_at)/count(ts_contact_at)::float) * 100 from airbnb_contacts)
+ 
+ select * from cte
