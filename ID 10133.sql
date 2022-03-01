6813c506f37ae2e4763c5801cd2bac02cf822247
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-03-01 10:07:44 -0500

    Create ID 10133.sql

---
+++ ID 10133.sql
@@ -0,0 +1,5 @@
+Medium Airbnb General Practice ID 10133
+Requests Acceptance Rate
+Find the acceptance rate of requests which is defined as the ratio of accepted contacts vs all contacts. Multiply the ratio by 100 to get the rate.
+
+select (count(ts_accepted_at)/count(ts_contact_at)::float) * 100 from airbnb_contacts
